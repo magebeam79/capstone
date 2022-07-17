807bc4c4b6870eae7404530635d417d8288253ec
Ericka Beam <105379067+magebeam79@users.noreply.github.com>
2022-07-17 18:20:53 -0400

    Update project.py
    Added error handling for improper year format. Included handling of years that are not in the database, i.e. requests for a valid year between x and y years. 
    
    Updated handling of sales graph to included adding the report year to the file name, and updated path to save in yearly reports directory.

--- project.py
+++ project.py
@@ -5,27 +5,70 @@ import glob
 import matplotlib.pyplot as plt
 import os
 import shutil
+import sys
+from datetime import datetime
+import re
+
 
 def main():
     year = input("Please enter a yearly report needed: ")
-    find_files = input_year(year)
+    check_year(year)
+    check_path(year)
+    find_files = read_files(year)
     find_totals = clean_data(find_files)
-    totals = (run_totals(find_totals))
-    print(run_totals(find_totals))
+    totals = run_totals(find_totals)
+    run_totals(find_totals)
     (yearly_report(totals, year))
     graph(year)
 
-def input_year(year):
+
+def check_year(year):
+    """verify that input year is four digits between 2020 and the current year"""
+    current_year = datetime.now().year
+    pattern = r'(^20[2][0-9]$)'
+
+    while True:
+        try:
+            year = int(year)
+            if 2020 > year or year > current_year:
+                sys.exit(f'There are no reports for that year. Please enter a year between 2020 and {current_year}.')
+            if re.match(pattern, str(year)):
+                print(f'Your report for year {year} will be processed shortly.')
+                break
+            else:
+                sys.exit(f'Please enter a four digit year between 2020 and {current_year}.')
+
+        except ValueError:
+            print("Please enter a valid four digit year.")
+            break
+
+
+def check_path(year):
+    """verify file path with given year"""
+    path = f"monthly_reports/*{year}.csv"
+
+    if glob.glob(path):
+        print(f"Files exist for {year}.")
+    else:
+        sys.exit(f"No files exist for {year}.")
+
+
+def read_files(year):
     """find sales reports for the inputted year"""
     find_files = pd.concat(map(pd.read_csv, glob.glob(f"monthly_reports/*{year}.csv")))
     return find_files
+
 
 def clean_data(find_files):
     """clean data, remove and format columns"""
     report_year = find_files
 
-    """remove columns that we will not be working with"""
-    report_year.drop(['Net amount', 'Type'], inplace=True, axis=1, errors='ignore')
+    """keep only the columns that are needed for the reports"""
+    report_year.drop(report_year.columns.difference(['Gross transaction amount', 'Transaction creation date',
+                                                     'Transaction amount', 'Shipping and handling',
+                                                     'Final Value Fee - fixed',
+                                                     'Final Value Fee - variable', 'Quantity', 'Order number',
+                                                     'Item title']), inplace=True, axis=1, errors='ignore')
 
     """clean the data; drop na and drop -- (empty columns)"""
     report_year = report_year[report_year != '--']
@@ -33,7 +76,7 @@ def clean_data(find_files):
 
     """rename column"""
     report_year.rename(columns={'Gross transaction amount': 'Transaction amount'}, inplace=True)
-    report_year.rename(columns={'Transaction creation date':'Date'}, inplace=True)
+    report_year.rename(columns={'Transaction creation date': 'Date'}, inplace=True)
 
     """remove comma from column"""
     report_year['Transaction amount'] = report_year['Transaction amount'].astype(str)
@@ -50,6 +93,7 @@ def clean_data(find_files):
     report_year['Date'] = pd.to_datetime(report_year['Date'])
 
     return report_year
+
 
 def run_totals(find_totals):
     totals = find_totals
@@ -74,23 +118,46 @@ def run_totals(find_totals):
     """group by Date sorted by gross transaction amount"""
     return totals.groupby(pd.Grouper(key='Date', axis=0, freq='M')).sum()
 
-def graph(year):
 
-    """configure bar graph for monthly sales for year"""
+def yearly_report(totals, year):
+    totals.reset_index(inplace=True)
+    year = str(year)
+
+    """return monthly sales for year as csv file"""
+    totals.to_csv('monthly_totals.csv')
+
+    """save the the report in the yearly reports directory with the correct report year"""
+    if os.path.isfile(f'yearly_reports/monthly_totals_{year}.csv'):
+        pass
+    else:
+        shutil.copy('monthly_totals.csv', 'yearly_reports/monthly_totals.csv')
+        os.rename('yearly_reports/monthly_totals.csv', f'yearly_reports/monthly_totals_{year}.csv')
+
+
+def graph(year):
+    """configure bar graph for monthly sales for year and input data"""
     data = pd.read_csv('monthly_totals.csv')
     df = pd.DataFrame(data)
 
     plt.rcParams['figure.figsize'] = [10, 6]
     plt.rcParams['figure.autolayout'] = True
     plt.tick_params(rotation=45)
-    plt.bar(df['Date'], df['Gross sales'], color='cyan', edgecolor = 'darkblue')
-    plt.title(f"Monthly Gross Sales\n{year}", fontsize = 16)
-    plt.xlabel("Month")
-    plt.ylabel("Sales in USD ($)")
+    plt.bar(df['Date'], df['Gross sales'], color='cyan', edgecolor='darkblue')
+    plt.title(f'Monthly Gross Sales\n{year}', fontsize=16)
+    plt.xlabel('Month')
+    plt.ylabel('Sales in USD ($)')
 
     """Save sales graph"""
-    plt.savefig(f"{year}_sales_graph.png")
+    plt.savefig('sales_graph.png')
     plt.show()
+
+    """save sales graph in the yearly reports directory with the correct year"""
+    if os.path.isfile(f'yearly_reports/sales_graph_{year}.png'):
+        pass
+    else:
+        shutil.copy('sales_graph.png', 'yearly_reports/sales_graph.png')
+        os.rename('yearly_reports/sales_graph.png', f'yearly_reports/sales_graph_{year}.png')
+
 
 if __name__ == '__main__':
     main()
